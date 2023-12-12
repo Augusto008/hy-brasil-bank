5e058dafb99fc88443291939e530c26c500dd61c
Augusto008 <aquila.real0@gmail.com>
2023-12-12 20:40:00 -0300

    Refactor `template.blade.php` code
    No changes in code it self, just in white spaces.

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('title')</title>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
 
-        <link rel="stylesheet" href="assets/css/styles.css">
-        
-        {{-- Ajax --}}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    
+    {{-- Ajax --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
 
-        {{-- Bootstrap --}}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.css" rel="stylesheet">
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.css" rel="stylesheet">
 
-    </head>
-    <body>
+  </head>
+  <body>
 
-        @include('layouts.gen.header')
+    @include('layouts.gen.header')
 
-        @yield('content')
+    @yield('content')
 
-        @include('layouts.gen.footer')
+    @include('layouts.gen.footer')
 
-        {{-- Bootstrap --}}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.js"></script>
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.js"></script>
 
-    </body>
+  </body>
 </html>
