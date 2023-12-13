b710cf67d2638d5cf096abbd1292dd45a4c5ae68
Augusto008 <aquila.real0@gmail.com>
2023-12-13 19:13:50 -0300

    Create component `banners/random.blade.php`
    Create 'banners/random.blade.php' containing a bootstrap slider.

---
+++ resources/views/components/banners/random.blade.php
@@ -0,0 +1,21 @@
+<section id="random-carousel" class="w-100 carousel carousel-dark slide" data-bs-touch="false" style="height: 250px;">
+  <div class="carousel-inner">
+    <div class="carousel-item active" style="background-image: url('{{asset('assets/images/praia-do-aventureiro-ilha-grande-shutterstockk.webp')}}'); background-repeate: no-repeate; background-position: center; background-position-y: 72%;');">
+      <img src="{{asset('assets/images/asset-utilization.png')}}" class="d-block m-auto" style="height: 250px;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('assets/images/bank-svgrepo-com.svg')}}" class="d-block m-auto" style="height: 250px;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('assets/images/bulletin-journal-magazine-svgrepo-com.svg')}}" class="d-block m-auto" style="height: 250px;" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#random-carousel" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#random-carousel" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</section>
