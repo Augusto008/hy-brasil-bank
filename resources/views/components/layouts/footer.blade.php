be6e03fdb6c01d3f78f947c70914b7f592829d21
Augusto008 <aquila.real0@gmail.com>
2023-12-13 19:21:29 -0300

    Create component `layouts/footer.blade.php`
    Create 'layouts/footer.blade.php' containing a basic model of a footer.

---
+++ resources/views/components/layouts/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="d-flex flex-column justify-content-between">
+  <div class="d-flex flex-column align-items-center">
+      <div id="social-media" class="m-5 px-5 col-8 d-flex justify-content-around">
+        <a href="">
+          <img src="{{asset('assets/images/facebook-svgrepo-com.svg')}}" alt="" width="40" height="40">
+        </a>
+        <a href="">
+          <img src="{{asset('assets/images/instagram-svgrepo-com.svg')}}" alt="" width="40" height="40">
+        </a>
+        <a href="">
+          <img src="{{asset('assets/images/youtube-svgrepo-com.svg')}}" alt="" width="40" height="40">
+        </a>
+        <a href="">
+          <img src="{{asset('assets/images/twitter-154-svgrepo-com.svg')}}" alt="" width="40" height="40">
+        </a>
+      </div>
+      <div id="footer-options" class="col-12 p-5 d-flex justify-content-around">
+        <a href="">Contact Us</a>
+        <a href="">Our Services</a>
+        <a href="">Privacy Policy</a>
+        <a href="">Terms & Conditions</a>
+      </div>
+  </div>
+  <div class="col-12 text-center my-5">
+      <a>HY BRASIL BANK Copyright © 2023 - All rights reserved || Designed By: Áquila A. A. Trindade</a>
+  </div>
+</footer>
