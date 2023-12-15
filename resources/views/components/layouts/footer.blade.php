0b2c8a2f0704f5b9d65c053469e0f982995a3d62
Augusto008 <aquila.real0@gmail.com>
2023-12-15 20:03:54 -0300

    Refactor(`footer.blade.php`): add divs and bootstrap classes
    Refactor the columns rules by adding some divs and using bootstrap classes ('.row', '.row-cols-' and '.col').
    Change the 'Twitter' icon to 'Twitter-x' icon.
    Remove unnecessary 'flex' classes.

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,24 +1,40 @@
 <footer class="d-flex flex-column justify-content-between">
   <div class="d-flex flex-column align-items-center">
-      <div id="social-media" class="m-5 px-5 col-8 d-flex justify-content-around">
-        <a href="">
-          <img src="{{asset('assets/images/facebook-svgrepo-com.svg')}}" alt="" width="40" height="40">
-        </a>
-        <a href="">
-          <img src="{{asset('assets/images/instagram-svgrepo-com.svg')}}" alt="" width="40" height="40">
-        </a>
-        <a href="">
-          <img src="{{asset('assets/images/youtube-svgrepo-com.svg')}}" alt="" width="40" height="40">
-        </a>
-        <a href="">
-          <img src="{{asset('assets/images/twitter-154-svgrepo-com.svg')}}" alt="" width="40" height="40">
-        </a>
+      <div id="social-media" class="m-5 px-5 col-md-8 col-10 row">
+        <div class="col">
+          <a href="">
+            <img class="icon-link p-0" src="{{asset('assets/images/facebook-svgrepo-com.svg')}}" alt="" width="40" height="40">
+          </a>
+        </div>
+        <div class="col">
+          <a href="">
+            <img class="icon-link" src="{{asset('assets/images/instagram-svgrepo-com.svg')}}" alt="" width="40" height="40">
+          </a>
+        </div>
+        <div class="col">
+          <a href="">
+            <img class="icon-link" src="{{asset('assets/images/youtube-svgrepo-com.svg')}}" alt="" width="40" height="40">
+          </a>
+        </div>
+        <div class="col">
+          <a href="">
+            <img class="icon-link" src="{{asset('assets/images/twitter-x-seeklogo.com.svg')}}" alt="" width="40" height="40">
+          </a>
+        </div>
       </div>
-      <div id="footer-options" class="col-12 p-5 d-flex justify-content-around">
-        <a href="">Contact Us</a>
-        <a href="">Our Services</a>
-        <a href="">Privacy Policy</a>
-        <a href="">Terms & Conditions</a>
+      <div id="footer-options" class="text-center row row-cols-md-4 row-cols-1 col-12 py-md-5 py-2 px-5">
+        <div class="col">
+          <a href="" class="footer-links lh-5 col">Contact Us</a>
+        </div>
+        <div class="col">
+          <a href="" class="footer-links lh-5 col">Our Services</a>
+        </div>
+        <div class="col">
+          <a href="" class="footer-links lh-5 col">Privacy Policy</a>
+        </div>
+        <div class="col">
+          <a href="" class="footer-links lh-5 col">Terms & Conditions</a>
+        </div>
       </div>
   </div>
   <div class="col-12 text-center my-5">
