36778c338c72924c3f6ef716d4b93582832a7bb9
Augusto008 <aquila.real0@gmail.com>
2023-12-15 19:50:44 -0300

    Feat(`random.blade.php`): add autoplay functionality
    Add bootstrap classes to create autoplay functionality in the random banner component.

--- resources/views/components/banners/random.blade.php
+++ resources/views/components/banners/random.blade.php
@@ -1,12 +1,12 @@
-<section id="random-carousel" class="w-100 carousel carousel-dark slide" data-bs-touch="false" style="height: 250px;">
+<section id="random-carousel" class="w-100 carousel slide carousel-dark slide" data-bs-ride="carousel" data-bs-touch="false" style="height: 250px;">
   <div class="carousel-inner">
-    <div class="carousel-item active" style="background-image: url('{{asset('assets/images/praia-do-aventureiro-ilha-grande-shutterstockk.webp')}}'); background-repeate: no-repeate; background-position: center; background-position-y: 72%;');">
+    <div class="carousel-item active" data-bs-interval="5000" style="background-image: url('{{asset('assets/images/praia-do-aventureiro-ilha-grande-shutterstockk.webp')}}'); background-repeate: no-repeate; background-position: center; background-position-y: 72%;');">
       <img src="{{asset('assets/images/asset-utilization.png')}}" class="d-block m-auto" style="height: 250px;" alt="...">
     </div>
-    <div class="carousel-item">
+    <div class="carousel-item" data-bs-interval="5000">
       <img src="{{asset('assets/images/bank-svgrepo-com.svg')}}" class="d-block m-auto" style="height: 250px;" alt="...">
     </div>
-    <div class="carousel-item">
+    <div class="carousel-item" data-bs-interval="5000">
       <img src="{{asset('assets/images/bulletin-journal-magazine-svgrepo-com.svg')}}" class="d-block m-auto" style="height: 250px;" alt="...">
     </div>
   </div>
