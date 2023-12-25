12c0f6e5279c545fc400adc77ec0114e76033f3c
Augusto008 <aquila.real0@gmail.com>
2023-12-25 20:17:03 -0300

    Refactor(`random.blade.php`): Change banners declaration for a loop
    Create an array of banners
    Create a foreach to declare the banners in sequence

--- resources/views/components/banners/random.blade.php
+++ resources/views/components/banners/random.blade.php
@@ -1,14 +1,21 @@
 <section id="random-carousel" class="w-100 carousel slide carousel-dark slide" data-bs-ride="carousel" data-bs-touch="false" style="height: 250px;">
+
   <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="5000" style="background-image: url('{{asset('assets/images/praia-do-aventureiro-ilha-grande-shutterstockk.webp')}}'); background-repeate: no-repeate; background-position: center; background-position-y: 72%;');">
-      <img src="{{asset('assets/images/asset-utilization.png')}}" class="d-block m-auto" style="height: 250px;" alt="...">
-    </div>
-    <div class="carousel-item" data-bs-interval="5000">
-      <img src="{{asset('assets/images/bank-svgrepo-com.svg')}}" class="d-block m-auto" style="height: 250px;" alt="...">
-    </div>
-    <div class="carousel-item" data-bs-interval="5000">
-      <img src="{{asset('assets/images/bulletin-journal-magazine-svgrepo-com.svg')}}" class="d-block m-auto" style="height: 250px;" alt="...">
-    </div>
+
+    @php
+      $data = [
+        ["praia-do-aventureiro-ilha-grande-shutterstockk.webp", "asset-utilization.png"],
+        ["", "bank-svgrepo-com.svg"],
+        ["", "bulletin-journal-magazine-svgrepo-com.svg"]
+      ];
+    @endphp
+
+    @foreach ($data as $dt)
+      <div class="carousel-item @if ($loop->first)active @endif" data-bs-interval="5000" style="background-image: url('{{ asset('assets/images/' . $dt[0]) }}'); background-repeat: no-repeat; background-position: center; background-position-y: 72%;">
+        <img src="{{ asset('assets/images/' . $dt[1]) }}" class="d-block m-auto" style="height: 250px;" alt="...">
+      </div>
+    @endforeach
+
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#random-carousel" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
