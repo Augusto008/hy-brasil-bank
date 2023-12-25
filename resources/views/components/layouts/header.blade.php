de64bb470a7d40a4bab9d7f18c9cb1382364a58f
Augusto008 <aquila.real0@gmail.com>
2023-12-25 20:29:13 -0300

    Update(`header.blade.php`): add id to a link and add bootstrap classes
    change bootstrap, in line 12, `text-center` to `text-end`
    Add an id and bootstrap classes to login button

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -9,7 +9,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="nav-menu">
-        <ul class="col-12 navbar-nav text-center me-auto mb-2 mb-lg-0 px-4">
+        <ul class="col-12 navbar-nav text-end me-auto mb-2 mb-lg-0 px-4">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
@@ -30,7 +30,7 @@
           </li>
           <li class="flex-fill"><hr role="separator" class="d-lg-none divider"></li>
           <li class="nav-item">
-            <a class="nav-link fw-bolder px-2 py-3" href="#">Login</a>
+            <a id="nav-login-btn" class="nav-link fw-bolder rounded-circle shadow-lg px-2 py-3" href="#">Login</a>
           </li>
         </ul>
       </div>
